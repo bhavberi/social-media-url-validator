a2e7b95682cde90c96e51ae28f3fad0dc786430a
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2024-07-05 14:30:22 +0530

    Added support for twitter/x (Renamed), and formatted the file

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-exports.validate = (platform, socialMediaUrl) =>  {
-    platform = platform.toLowerCase()
-    const regexMap = {
-        facebook:
-		/^(https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-]+\.?)(?:\/)?/i,
-	    instagram: /^(http(s)?:\/\/)?(?:www\.)?instagram\.com\/([a-zA-Z0-9_]+)/,
-	    twitter: /^(http(s)?:\/\/)?(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/,
-	    linkedin:
+exports.validate = (platform, socialMediaUrl) => {
+  platform = platform.toLowerCase();
+  const regexMap = {
+    facebook:
+      /^(https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w-]*\/)*([\w\-]+\.?)(?:\/)?/i,
+    instagram: /^(http(s)?:\/\/)?(?:www\.)?instagram\.com\/([a-zA-Z0-9_]+)/,
+    twitter: /^(http(s)?:\/\/)?(?:www\.)?(twitter|x)\.com\/([a-zA-Z0-9_]+)/,
+    x: /^(http(s)?:\/\/)?(?:www\.)?(twitter|x)\.com\/([a-zA-Z0-9_]+)/,
+    linkedin:
       /^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile|company)\/([a-zA-Z0-9_]+)/,
-	    emailaddress: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
-        website: /^((https?):\/\/)?([w|W]{3}\.)+[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/
+    emailaddress: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+    website:
+      /^((https?):\/\/)?([w|W]{3}\.)+[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/,
+  };
+  let isValid = false;
+  if (regexMap[platform]) {
+    if (regexMap[platform].test(socialMediaUrl)) {
+      isValid = true;
     }
-    let isValid = false
-    if (regexMap[platform]){
-        if (regexMap[platform].test(socialMediaUrl)) {
-            isValid = true
-        }
-    }
+  }
 
-    return isValid
-
-}
+  return isValid;
+};
